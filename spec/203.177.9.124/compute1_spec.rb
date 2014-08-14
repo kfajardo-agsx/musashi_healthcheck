be7de28618bf30284e123820bd9d5a0beb60c072
ceilomar documento <ceilo.doc143@gmail.com>
2014-08-14 17:05:11 +0800

    added ports to check, complete list of package and services to check

--- spec/203.177.9.124/compute1_spec.rb
+++ spec/203.177.9.124/compute1_spec.rb
@@ -1,14 +1,87 @@
 require 'spec_helper'
 
-# CHECK DEFAULT PORTS
+# CHECK PACKAGES
+describe package('python-guestfs')  do
+  it { should be_installed }
+end
+
+describe package('nova-network')  do
+  it { should be_installed }
+end
+
+describe package('nova-api-metadata')  do
+  it { should be_installed }
+end
+
+describe package('nova-compute-kvm')  do
+  it { should be_installed }
+end
+
+# CHECK SERVICES
+
+describe service('nova-compute') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
+describe service('nova-api-metadata') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
+describe service('nova-network') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
+# CHECK PORTS
+
+#python
 describe port (8775) do
   it { should be_listening.with('tcp') }
 end
 
-#describe port (53) do
-#  it { should be_listening.with('tcp') }
-#end
+#dnsmasq
+describe port (53) do
+  it { should be_listening.with('tcp') }
+end
 
+#sshd
 describe port (22) do
   it { should be_listening.with('tcp') }
+end
+
+#qemu-system-x
+describe port (5975) do
+	it { should be_listening.with('tcp')}
+end
+
+#qemu-system-x
+describe port (5900) do
+	it { should be_listening.with('tcp')}
+end
+
+#qemu-system-x8
+describe port (5904) do
+	it { should be_listening.with('tcp')}
+end
+
+#python
+describe port (41807) do
+	it { should be_listening.with('udp')}
+end
+
+#ntpd
+describe port (123) do
+	it { should be_listening.with('udp')}
+end
+
+#python
+describe port (60692) do
+	it { should be_listening.with('udp')}
+end
+
+#rsyslogd
+describe port (57573) do
+	it { should be_listening.with('udp')}
 end
