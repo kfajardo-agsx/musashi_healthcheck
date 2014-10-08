296325d96d2dc8ad6e1b38bb2d2b4f8ede1063b3
Caroline Jandayan <cjandayan@musashi.ph>
2014-10-08 14:30:00 +0800

    KEN-1339 OCT 8 2:29PM

---
+++ spec/NAME.OR.IP.OF.SNMASTER.HERE/SNmaster_spec.rb
@@ -0,0 +1,109 @@
+require 'spec_helper'
+require 'rspec/its'
+
+# CHECK SERVICES
+describe service('diamond')  do
+  it { should be_running }
+end
+
+describe service('supervisord') do
+  it { should be_running }
+end
+
+describe service('graphite') do
+  it { should be_running   }
+end
+
+describe service('gunicorn_django') do
+  it { should be_running }
+end
+
+describe service('honcho') do
+  it { should be_running }
+end
+
+describe service('gunicorn') do
+  it { should be_running   }
+end
+
+
+# CHECK DEFAULT PORTS
+describe port (6804) do   # ceph-osd
+  it { should be_listening.with('tcp') }
+end
+
+describe port (6805) do   # ceph-osd
+  it { should be_listening.with('tcp') }
+end
+
+describe port (22) do   # sshd
+  it { should be_listening.with('tcp') }
+end
+
+describe port (6789) do   # ceph-mon
+  it { should be_listening.with('tcp') }
+end
+
+describe port (8300) do   # consul
+  it { should be_listening.with('tcp') }
+end
+
+describe port (8301) do   # consul
+  it { should be_listening.with('tcp') }
+end
+
+describe port (8302) do   # consul
+  it { should be_listening.with('tcp') }
+end
+
+describe port (6800) do   # ceph-osd
+  it { should be_listening.with('tcp') }
+end
+
+describe port (6801) do   # ceph-osd
+  it { should be_listening.with('tcp') }
+end
+
+describe port (6802) do   # ceph-osd
+  it { should be_listening.with('tcp') }
+end
+
+describe port (6803) do   # ceph-osd
+  it { should be_listening.with('tcp') }
+end
+
+describe port (8500) do   # consul
+  it { should be_listening.with('tcp6') }
+end
+
+describe port (8600) do   # consul
+  it { should be_listening }
+end
+
+describe port (8400) do   # consul
+  it { should be_listening }
+end
+
+describe port (2004) do   # docker: acaleph-frontend/graphite,graphite
+  it { should be_listening }
+end
+
+describe port (7002) do   # docker : acaleph-frontend/graphite,graphite
+  it { should be_listening }
+end
+
+describe port (8126) do   # docker : acaleph-frontend/graphite,graphite
+  it { should be_listening }
+end
+
+describe port (8080) do   # docker: acaleph-frontend/graphite,graphite
+  it { should be_listening }
+end
+
+describe port (8081) do   # docker: acaleph-frontend/graphite,graphite
+  it { should be_listening }
+end
+
+describe port (8082) do   # docker: acaleph-frontend/graphite,graphite
+  it { should be_listening }
+end
