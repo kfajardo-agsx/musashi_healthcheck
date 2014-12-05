4d4a1757207a5a4694868c06af099ecc38b9e3ce
ceilomar documento <ceilo.doc143@gmail.com>
2014-12-05 19:33:04 +0800

    v1.1.0 sprint4-b14 serverspec

--- spec/CN2/module_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'rspec/its'
-
-#VLAN-configured and bonded
-describe file('/etc/modules') do
-  its(:content) { should match /^bonding/ }
-  its(:content) { should match /^8021q/ }
-end
