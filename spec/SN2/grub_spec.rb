7544103f6781ea9d5807b1c7a83295aa03a419d5
ceilomar documento <ceilo.doc143@gmail.com>
2014-12-05 20:08:32 +0800

    sprint4 serverspecs

--- spec/SN2/grub_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-require 'rspec/its'
-
-describe file('/etc/default/grub') do
-  its(:content) { should match /^GRUB_RECORDFAIL_TIMEOUT=30/ }
-  its(:content) { should match /^GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS1,115200"/ }
-end
